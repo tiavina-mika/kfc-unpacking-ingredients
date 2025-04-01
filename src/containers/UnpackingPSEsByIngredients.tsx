31c631657583216ba0e3722ef6a7b3a0dcbcc3d6
tiavina-mika <tiavinamika@gmail.com>
2025-04-01 10:52:35 +0300

    Create UnpackingPSEsByIngredients.tsx

---
+++ src/containers/UnpackingPSEsByIngredients.tsx
@@ -0,0 +1,55 @@
+import { Box, Tab, Tabs } from "@mui/material";
+import { ingredientsDone, ingredientsTodo } from "../utils/data";
+import { useState } from "react";
+import UnpackingPSEsByIngredientsForm from "./UnpackingPSEsByIngredientsForm";
+
+const sx = {
+  tabsContainer: {
+    borderBottom: 1,
+    borderColor: 'divider',
+    display: "flex"
+  },
+  tabs: {
+    flex: 1,
+    display: "flex",
+  },
+  tab: {
+    flex: 1,
+    textAlign: "center",
+  }
+}
+const UnpackingPSEsByIngredients = () => {
+  const [tab, setTab] = useState<number>(0);
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={sx.tabsContainer}>
+        {[`Produits à DÉConditionner (${ingredientsTodo.length})`, `PRODUITS DÉConditionnés (${ingredientsDone.length})`].map((title, index) => (
+          <Tabs
+            key={index}
+            value={index}
+            textColor="inherit"
+            indicatorColor="primary"
+            onChange={handleChange}
+            sx={sx.tabs}
+            aria-label="Unpacking Ingredients Tabs"
+          >
+            <Tab
+              label={title}
+              id={`unpacking-ingredients-${index}`}
+              aria-controls={`unpacking-ingredientspanel-${index}`}
+              sx={sx.tab}
+            />
+          </Tabs>
+        ))}
+      </Box>
+      <UnpackingPSEsByIngredientsForm ingredients={ingredientsTodo} tab={tab} index={0} />
+      <UnpackingPSEsByIngredientsForm ingredients={ingredientsDone} tab={tab} index={1} />
+    </Box>
+  )
+}
+
+export default UnpackingPSEsByIngredients;
