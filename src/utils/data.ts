81a5c47e8687b7fed5b387dff5db7787340789d0
tiavina-mika <tiavinamika@gmail.com>
2025-04-01 10:52:43 +0300

    Create data.ts

---
+++ src/utils/data.ts
@@ -0,0 +1,43 @@
+export const ingredientsTodo = [
+    {
+        "name": "X-Coriandre",
+        "objectId": "59uhvzW188",
+        "productionStepExecutions": [
+            {
+                "objectId": "lJNaT3R5RW",
+                "recipeName": "Rice bowl aux aubergines miso - 5881",
+                "uniqueCode": "B87",
+                "theoreticalGrossWeight": 2,
+                "theoreticalNetWeight": 2,
+                "grossWeight": 2,
+                "description": "mettre la coriandre dans le bac 2",
+            }
+        ]
+    }
+  ]
+
+export const ingredientsDone = [
+    {
+        "name": "X-Brocolis",
+        "objectId": "YARn2QGemM",
+        "productionStepExecutions": [
+            {
+                "objectId": "DXfMpHaSFN",
+                "recipeName": "Spring bowl aux asperges vertes & oeuf poché - 10385",
+                "uniqueCode": "L76",
+                "theoreticalGrossWeight": 0.51,
+                "theoreticalNetWeight": 0.5,
+                "grossWeight": 2,
+                "description": "mettre les brocolis dans le bac 1",
+            },
+            {
+                "objectId": "raMkJs1uCj",
+                "recipeName": "Rice bowl aux aubergines miso - 5881",
+                "uniqueCode": "B87",
+                "theoreticalGrossWeight": 1,
+                "theoreticalNetWeight": 1,
+                "grossWeight": 2,
+            }
+        ]
+    }
+]
